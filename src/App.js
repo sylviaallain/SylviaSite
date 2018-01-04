47f34e2a4ad2a22440bc123c0ee07c44a209e4f6
Sylvia Allain <SAllain@carbonite.com>
2018-01-04 22:08:22 +0000

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./app/components/home/home.js";
+import AboutMe from "./app/components/about-me/about-me.js";
+import Contact from "./app/components/contact/contact.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Sylvia Allain</h1>
-        </header>
-        <body className="App-body">
-
-        </body>
-      </div>
+      <HashRouter>
+        <div className="app">
+          <header className="header">
+            <h1 className="title">Sylvia Allain</h1>
+            <ul className="navigation">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/aboutme">About Me</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+          </header>
+          <body className="body">
+            <Route exact path="/" component={Home}/>
+            <Route path="/aboutme" component={AboutMe}/>
+            <Route path="/contact" component={Contact}/>
+          </body>
+        </div>
+      </HashRouter>
     );
   }
 }
--- src/app/components/about-me/about-me.js
+++ src/app/components/about-me/about-me.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 export default class AboutMe extends Component {
   render() {
     return (
-      <div className="AboutMe">
-        About me!
+      <div>
+      <h2>STUFF</h2>
+      <p>Mauris sem velit, vehicula eget sodales vitae,
+      rhoncus eget sapien:</p>
+      <ol>
+        <li>Nulla pulvinar diam</li>
+        <li>Facilisis bibendum</li>
+        <li>Vestibulum vulputate</li>
+        <li>Eget erat</li>
+        <li>Id porttitor</li>
+      </ol>
       </div>
     )
   }
