21ba944c4d8e55728deadc0e0afd164f7c929706
Sylvia Allain <SAllain@carbonite.com>
2018-01-04 21:18:29 +0000

    Base site layout

---
+++ src/app/components/about-me/about-me.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AboutMe extends Component {
+  render() {
+    return (
+      <div className="AboutMe">
+        About me!
+      </div>
+    )
+  }
+}
